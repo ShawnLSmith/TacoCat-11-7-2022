67f1af55340402f8c51e2d14f4ffa770b8414cee
ShawnLSmith <shawn@rsatechnologyservices.com>
2022-12-07 22:16:04 -0600

    Finished TacoCat Video Lesson (12/7/2022)
    Added Taco Cat Logos designed in Canva.
    Added Logos to appropriate sections in the site.
    Made changes to Script section for button event to work.
    Commenced Deep Dive on the JavaScript code with dad to help me understand it better.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,53 @@
-//Call HelloWorld
-function HelloWorld(){
-    alert("Hello World");
+//Get our user input
+function getValue(){
+
+    //make sure the alert is invisible
+    document.getElementById("alert").classList.add("invisible");
+
+    //get user string for the page
+    let userString = document.getElementById("userString").value;
+
+    //check for a Palindrome 
+    let returnObj = checkForPalindrome(userString);
+
+    //display out message to the screen
+    displayMessage(returnObj);
+
+}
+
+//Check if string is a Palindrome.
+function checkForPalindrome(userString) {
+
+    //To Lower Case
+    userString = userString.toLowerCase();
+
+    //remove spaces and special characters.
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex,"");
+
+    let revString = [];
+    let returnObj = {};
+
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];        
+    }
+
+    if (revString == userString) {
+        returnObj.msg = "Well Done! You entered a Palindrome!"
+    }
+    else{
+        returnObj.msg = "Sorry! You did not enter a Palindrome!"
+    }
+
+    returnObj.reversed = revString;
+
+    return returnObj;
+}
+
+// Display a message to the string.
+function displayMessage(returnObj) {
+
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed} `;
+    document.getElementById("alert").classList.remove("invisible");
 }
